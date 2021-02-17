691d88b5e18728240f8e7a85e4b4e0939757d6bf
Pavel Soloshenko <apodisation13@gmail.com>
2021-02-17 13:04:51 +0300

    dealer logic: hit, stand, blackjack check

--- try_1.py
+++ try_1.py
@@ -33,7 +33,7 @@ def calc_cards(hand: list):
             calc_hand.append(11)
         else:
             calc_hand.append(card)
-    print(calc_hand)
+    # print(calc_hand)
     return calc_hand
 
 
@@ -43,24 +43,29 @@ def check_blackjack(hand: list, player: str):
     result = sum(hand)
     if result == 21:
         if player == "Dealer":
-            print(f'Карты в руке дилера: {card_hand}')
-            print(f'21!!! {player} wins the game!')
+            print(f'Карты в руке дилера: {hand}')
+            print("\033[31m{}\033[0m".format("BLACKJACK!!! Dealer wins, you lose!"))
             return True
         elif player == "Player":
-            print(f'21!!! {player} wins the game!')
+            print("\033[32m{}\033[0m".format('BLACKJACK!!! You win the game!'))
             return True
     return False
 
 
-def check_score(hand: list):
+def check_score(hand: list, player: str):
     """проверяем счёт"""
     result = sum(hand)
-    print(f"Ваш результат - {result}")
+    # print(f"Ваш результат - {result}")
     if result > 21:
-        print(f'BUSTED! You lose the game')
-        return result
+        if player == "Player":
+            print("\033[31m{}\033[0m".format("BUSTED! You lose the game!"))
+        elif player == "Dealer":
+            print("\033[32m{}\033[0m".format('DEALER BUSTED! You win the game!'))
     elif result == 21:
-        print(f'21!!! You win the game!')
+        if player == "Player":
+            print("\033[32m{}\033[0m".format('BLACKJACK!!!'))
+        elif player == "Dealer":
+            print("\033[31m{}\033[0m".format('BLACKJACK!!! Dealer wins, you lose!'))
     return result
 
 
@@ -69,6 +74,7 @@ def hit(cards: list, hand: list):
     player_choice = choice(cards)
     cards.remove(player_choice)
     hand.append(player_choice)
+    print(hand)
     return hand
 
 
@@ -98,11 +104,23 @@ if not check_blackjack(calc_dealer_hand, "Dealer"):
             if command.lower() in ["hit", 'h']:
                 player_hand = hit(cards, player_hand)
                 calc_player_hand = calc_cards(player_hand)
-                player_result = check_score(calc_player_hand)
-                if player_result > 21:
+                player_result = check_score(calc_player_hand, "Player")
+                print(f'Ваш результат - {sum(calc_player_hand)}')
+                if player_result >= 21:
                     break
 
             elif command.lower() in ["stand", "s"]:
-                player_result = check_score(calc_player_hand)
-                dealer_result = check_score(calc_dealer_hand)
+                player_result = check_score(calc_player_hand, "Player")
+                dealer_result = check_score(calc_dealer_hand, "Dealer")
+                while dealer_result < player_result:
+                    dealer_hand = hit(cards, dealer_hand)
+                    calc_dealer_hand = calc_cards(dealer_hand)
+                    dealer_result = check_score(calc_dealer_hand, "Dealer")
+                print(f'Игрок: {player_hand} - {player_result}, Дилер: {dealer_hand} - {dealer_result}')
+                if dealer_result >= 21:
+                    break
+                elif player_result == dealer_result:
+                    print("НИЧЬЯ")
+                elif player_result < dealer_result:
+                    print("\033[31m{}\033[0m".format('Dealer wins, you lose!'))
                 break
