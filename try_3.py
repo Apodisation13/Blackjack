72cb2191a33738e5b0673db64d7d09f45b7aba9a
Pavel Soloshenko <apodisation13@gmail.com>
2021-02-18 10:52:11 +0300

    func calc_card got another file

--- try_3.py
+++ try_3.py
@@ -1,4 +1,5 @@
 from random import choice
+from calc_card import calc_cards
 
 
 def starting_draw_dealer(cards: list):
@@ -24,65 +25,21 @@ def starting_draw_player(cards: list):
     return starting_player_hand
 
 
-def calc_cards(hand: list, first_draw: bool):
-
-    calc_hand = []
-    result = 0
-    result_ace = 0
-    ace = False
-
-    if hand == ["A", "A"]:
-        # print("два туза в стартовой руке")
-        return 12, 0
-
-    ace_count = hand.count("A")
-    # print("количество тузов", ace_count)
-
-    for card in hand:
-        if card in ['J', 'Q', 'K']:
-            calc_hand.append(10)
-        elif card != "A":
-            calc_hand.append(card)
-        elif card == "A" and first_draw:
-            calc_hand.append(11)
-
-    # print("сумма руки без туза", sum(calc_hand))
-
-    if ace_count > 0 and not first_draw:
-        ace = True
-        if ace_count == 1:
-            result += ace_count
-            result_ace += 11
-        elif ace_count == 2:
-            result += ace_count
-            result_ace += 12
-        elif ace_count == 3:
-            result += ace_count
-            result_ace += 13
-        elif ace_count == 4:
-            result += ace_count
-            result_ace += 14
-
-    if ace:
-        result_ace += sum(calc_hand)
-        # print("Результат с тузами", result_ace)
-        if result_ace > 21:
-            result_ace = 0
-
-    result += sum(calc_hand)
-    return result, result_ace
-
-
 def hit(cards: list, hand: list):
     """кто-то берет 1 карту"""
     player_choice = choice(cards)
     cards.remove(player_choice)
     hand.append(player_choice)
+    print(f'hit {player_choice}')
     return hand
 
 
 def check_score(result: tuple):
+    print(f'Результат игрока {result[0]}')
     if result[1]:
+        if result[1] == 21:
+            print("BLACKJACK!!!")
+            return result[1]
         print(f'Результат игрока {result[0]}/{result[1]}')
     else:
         if result[0] > 21:
@@ -125,8 +82,7 @@ else:
         if command.lower() in ["hit", 'h']:
             hit(cards, player_hand)
             player_result = calc_cards(player_hand, first_draw=False)
-            print(player_hand, player_result[0])
+            print(f'Карты в руке игрока: {player_hand}')
             player_result = check_score(player_result)
-            print(player_result)
             if player_result >= 21:
                 break
